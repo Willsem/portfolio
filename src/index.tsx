065fb8c9e239b50b50510065031aea7885f1c0a3
Willsem <happymainer@gmail.com>
2021-10-22 16:46:48 +0300

    replace font

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/home/Home';
 import reportWebVitals from './reportWebVitals';
+import './fonts/Montserrat/Montserrat-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>

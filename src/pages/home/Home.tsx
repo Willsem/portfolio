ae6c41d6b7dbc620b0bbfc230484e40b8b449e16
Willsem <happymainer@gmail.com>
2021-10-22 15:49:58 +0300

    make src struct

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Willsem</p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
